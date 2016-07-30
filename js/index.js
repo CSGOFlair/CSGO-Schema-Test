b3779bb7729163afa44eeaf8f91b021405810cfb
Billard <sirfrancisbillard@gmail.com>
2016-07-30 02:35:14 -1000

    Durr

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Reliant on jQueer
-$.ajax({url:'your-url', success:function(data)
+$.ajax({url:'https://raw.githubusercontent.com/SteamDatabase/SteamTracking/master/ItemSchema/CounterStrikeGlobalOffensive.vdf', success:function(data)
 {
     var schema = JSON.parse(data);
     var test = schema.items_game.rarities.default.drop_sound;
