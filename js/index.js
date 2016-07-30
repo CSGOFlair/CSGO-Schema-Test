7e6ecc8eb4d5380f8092adb59c4e654afb52c3b1
Billard <sirfrancisbillard@gmail.com>
2016-07-30 02:39:06 -1000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 // Reliant on jQueer
-$.ajax({url:'https://raw.githubusercontent.com/SteamDatabase/SteamTracking/master/ItemSchema/CounterStrikeGlobalOffensive.vdf', success:function(data)
+function start()
 {
-    var schema = JSON.parse(data);
-    var test = schema.items_game.rarities.default.drop_sound;
-    document.getElementById("drop").innerHTML = data;
-}});
+    $.ajax({url:'https://raw.githubusercontent.com/SteamDatabase/SteamTracking/master/ItemSchema/CounterStrikeGlobalOffensive.vdf',     success:function(data)
+    {
+        var schema = JSON.parse(data);
+        var test = schema.items_game.rarities.default.drop_sound;
+        document.getElementById("drop").innerHTML = test || data;
+    }});
+}
