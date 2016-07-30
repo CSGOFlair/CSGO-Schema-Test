3a4a596a36e42c8b71f3f2d3032a31a80b7a5829
Billard <sirfrancisbillard@gmail.com>
2016-07-30 02:24:15 -1000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // Reliant on jQueer
 $.ajax({url:'your-url', success:function(data)
 {
-var schema = JSON.parse(data);
+    var schema = JSON.parse(data);
+    var test = schema.items_game.rarities.default.drop_sound;
+    document.getElementById("drop").innerHTML = test;
 }});
