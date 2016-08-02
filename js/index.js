fae29a0274637ae33ff4aede89ba6e838eedcca4
Billard <sirfrancisbillard@gmail.com>
2016-08-01 17:06:00 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ function start()
 {
 	$.ajax({url:"https://raw.githubusercontent.com/SteamDatabase/SteamTracking/master/ItemSchema/CounterStrikeGlobalOffensive.vdf", success:function(data)
 	{
-		var schema = JSON.parse(data);
-		var test = schema["items_game"].rarities.default.drop_sound;
+		var schema = VDF.parse(data);
+		var test = schema.items_game.rarities.default.drop_sound;
 		document.getElementById("drop").innerHTML = test || data || "ugh";
 	}});
 }
