ba0caab06980cce2dcf3efe523fd608cc10fd96d
Billard <sirfrancisbillard@gmail.com>
2016-08-12 17:12:53 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Reliant on jQueer
 $.ajax({url:'your-url', success:function(data)
 {
-    var schema = JSON.parse(data);
-    var test = schema.items_game.rarities.default.drop_sound;
-    document.getElementById("drop").innerHTML = test;
+	var schema = JSON.parse(data);
+	var test = schema.items_game.rarities.default.drop_sound;
+	document.getElementById("drop").innerHTML = test;
 }});
